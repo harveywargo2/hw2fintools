2d2747e4418e8d991f9b720a5c7d5b418113b215
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2026-02-25 15:18:27 -0500

    Update utils.py

--- hw2fintools/utils.py
+++ hw2fintools/utils.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 from hw2fintools import mod_path
+import datetime
 
 
 xlsx_path = os.path.join(mod_path, 'xlsx')
@@ -10,3 +11,10 @@ def get_df_stock_watch_list():
     df = pd.read_excel(os.path.join(xlsx_path, "stock_watch_list.xlsx"))
     return df
 
+
+def to_fiscal_year(date, fiscal_end_month):
+    if date.month > fiscal_end_month:
+        return date.year + 1
+
+    else:
+        return date.year
